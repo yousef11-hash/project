e02c2582785a0678e06a7f4fd36a8a59a84061f1
yousef11-hash <youssef.emad@meti.dev>
2024-12-30 19:04:01 +0200

    Add files via upload

---
+++ first.py
@@ -0,0 +1,4 @@
+
+print("Hello, my name is [Your Name].")
+print("I am [Your Age] years old.")
+print("In this project, I want to learn the basics of Python programming, including variables, functions, and control flow.")
